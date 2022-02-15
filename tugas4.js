f07b0dc544b66bace64dc2b4ee889e353aaa04d3
irf-dev <irfan.4444.maulana@gmail.com>
2022-02-15 21:01:46 +0700

    tugas 4 javascript
    penyerahan tugas

---
+++ tugas4.js
@@ -0,0 +1,20 @@
+var budi = 170;
+var dinda = 165;
+var tono = 175;
+
+if (budi > dinda) {
+    if (budi > tono) {
+        console.log("tinggi badan Budi paling tinggi");
+    }
+    else {
+        console.log("tinggi badan Budi tertinggi kedua");
+    }
+}
+else {
+    if (budi < tono) {
+        console.log("tinggi badan Budi terpendek");
+    }
+    else {
+        console.log("Budi terpendek setelah Tono");
+    }
+}
